abeafa4fc5464f35cdec1c5771c99b3e3c10e15c
Daniel <danielbebon@gmail.com>
2021-10-10 21:38:37 -0500

    Update Main calculator.c

--- Main calculator.c
+++ Main calculator.c
@@ -4,16 +4,13 @@ int main ()
 	//Operaciones aritmeticas basicas
 	int operacion, opc;
 	float num1, num2, suma1, resta1, multiplicacion1, division1;
-	
 	do{
 		printf("!!!Bienvenido!!!");
 		printf("\nIntroduzca su primer valor: ");
 		scanf("%f", &num1);
 		printf("\nIntroduzca su segundo valor: ");
 		scanf("%f", &num2);
-		suma1= num1+num2;resta1= num1-num2;
-		multiplicacion1= (num1)*(num2);division1= num1/num2;
-		
+		suma1= num1+num2;resta1= num1-num2;multiplicacion1= (num1)*(num2);division1= num1/num2;
 		printf("\nEscriba que operacion desea realizar\nSuma = 1\nResta = 2\nMultiplicacion = 3\nDivision = 4\n-> ");
 		scanf("%d", &operacion);
 		if (operacion==1){
