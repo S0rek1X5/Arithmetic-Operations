960cc9534e17fac2066f89cb9662e26788d43c1a
Daniel <danielbebon@gmail.com>
2021-10-21 10:02:04 -0500

    Funciones
    Actualización de código.
    -Mejora estética
    -Funciones

--- C++/Calculator.cpp
+++ C++/Calculator.cpp
@@ -1,39 +1,60 @@
 #include <iostream>
 #include<math.h>
 using namespace std;
+void Suma(float a,float b);
+void Resta(float a,float b);
+void Multiplicacion(float a,float b);
+void Division(float a,float b);
+void Raiz(float a,float b);
 int main(){
 	int operacion,opc;
-	float fst,sec,suma,resta,multip,divi,raiz;
-	cout<<"------BIENVENIDO------"<<endl;
+	float fst,sec,suma,resta,multip,divi;
+	cout<<"\t------BIENVENIDO------"<<endl;
 	do{
 		cout<<"Digite su primer valor: "<<endl;cin>>fst;
 		cout<<"Digite su segundo valor: "<<endl;cin>>sec;
-		cout<<"Que desea realizar?\nSuma - 1  Resta - 2\nMultiplicacion - 3  Division - 4\n Raiz cuadrada - 5: ";cin>>operacion;
-		if(operacion==1){
-			suma=fst+sec;
-			cout<<"Resultado de su suma es: "<<suma;
-		}
-		else if(operacion==2){
-			resta=fst-sec;
-			cout<<"Resultado de su resta es: "<<resta;
-		}
-		else if(operacion==3){
-			multip=fst*sec;
-			cout<<"Resultado de su multiplicacion: "<<multip;
-		}
-		else if(operacion==4){
-			divi=fst/sec;
-			cout<<"Resultado de su division: "<<divi;
-		}
-		else if(operacion==5){
-			raiz=sqrt(fst)+sqrt(sec);
-			cout<<"Resultado de la suma de la raiz: "<<raiz;
-		}
-		else{
-			cout<<"------ERROR------\nRecuerde digitar solo las opciones dadas en el programa";
-		}
+		cout<<"\tQue desea realizar?\nSuma - 1  Resta - 2\nMultiplicacion - 3  Division - 4\nRaiz cuadrada - 5: ";cin>>operacion;
+		switch(operacion){
+			case 1:
+				Suma(fst,sec);break;
+			case 2:
+				Resta(fst,sec);break;
+			case 3:
+				Multiplicacion(fst,sec);break;
+			case 4:
+				Division(fst,sec);break;
+			case 5:
+				Raiz(fst,sec);break;
+			default:
+				cout<<"------ERROR------\nRecuerde digitar solo las opciones dadas en el programa";
+		} 
 		cout<<"\nDigite 1 para VOLVER A INTENTARLO - Digite 2 para SALIR: ";cin>>opc;
 		cout<<"\n---------------------------------------------------------\n";
 	}while(opc==1);
 	return 0;
+}
+void Suma(float a,float b){
+	float result;
+	result=a+b;
+	cout<<"Su resultado es: "<<result;
+}
+void Resta(float a,float b){
+	float result;
+	result=a-b;
+	cout<<"Su resultado es: "<<result;
+}
+void Multiplicacion(float a,float b){
+	float result;
+	result=a*b;
+	cout<<"Su resultado es: "<<result;
+}
+void Division(float a,float b){
+	float result;
+	result=a/b;
+	cout<<"Su resultado es: "<<result;
+}
+void Raiz(float a,float b){
+	float raiz_1,raiz_2;
+	raiz_1=sqrt(a);raiz_2=sqrt(b);
+	cout<<""<<raiz_1<<" Y "<<raiz_2;
 }
