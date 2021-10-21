0e682cee848390daa68fcbd8a946a20dd742c920
Daniel <danielbebon@gmail.com>
2021-10-20 23:41:26 -0500

    Update Main calculator.c

--- Main calculator.c
+++ Main calculator.c
@@ -1,35 +1,51 @@
 #include <stdio.h>
-int main ()
-{
-	//Operaciones aritmeticas basicas
-	int operacion, opc;
-	float num1, num2, suma1, resta1, multiplicacion1, division1;
+void Suma(float a,float b);
+void Resta(float a,float b);
+void Multiplicacion(float a,float b);
+void Division(float a,float b);
+int main (){
+	int operacion,opc;
+	float num1,num2;
 	do{
-		printf("!!!Bienvenido!!!");
-		printf("\nIntroduzca su primer valor: ");
-		scanf("%f", &num1);
-		printf("\nIntroduzca su segundo valor: ");
-		scanf("%f", &num2);
-		suma1= num1+num2;resta1= num1-num2;multiplicacion1= (num1)*(num2);division1= num1/num2;
+		printf("\t!!!Bienvenido!!!");
+		printf("\nIntroduzca su primer valor: ");scanf("%f", &num1);
+		printf("\nIntroduzca su segundo valor: ");scanf("%f", &num2);
 		printf("\nEscriba que operacion desea realizar\nSuma = 1\nResta = 2\nMultiplicacion = 3\nDivision = 4\n-> ");
 		scanf("%d", &operacion);
-		if (operacion==1){
-			printf("\nEl resultado de su suma es: 1.2f", suma1);
-		}
-		else if(operacion==2){
-			printf("\nEl resultado de su resta es: 1.2f", resta1);
-		}
-		else if(operacion==3){
-			printf("\nEl resultado de su multiplicacion es: 1.2f", multiplicacion1);
-		}
-		else if(operacion==4){
-			printf("\nEl resultado de su division es: %1.2f", division1);
-		}
-		else{
-			printf("\nLa operacion no ha sido escrita correctamente o no esta dentro de las opciones del algoritmo");
+		switch(operacion){
+			case 1:
+				Suma(num1,num2);break;
+			case 2:
+				Resta(num1,num2);break;
+			case 3:
+				Multiplicacion(num1,num2);break;
+			case 4:
+				Division(num1,num2);break;
+			default:
+				printf("\nDigite una de las opciones disponibles");
 		}
 		printf("\nDigite 1 para CONTINUAR --- 2 para SALIR\n: ");scanf("%d",&opc);
 		printf("----------------------------------\n");
 	}while(opc==1);
 	return 0;
+}
+void Suma(float a,float b){
+	float result;
+	result=a+b;
+	printf("\nEl resultado de su suma es: %1.2f",result);
+}
+void Resta(float a,float b){
+	float result;
+	result=a-b;
+	printf("\nEl resultado de su resta: %1.2f",result);
+}
+void Multiplicacion(float a,float b){
+	float result;
+	result=a*b;
+	printf("\nEl resultado de su Multiplicacion es: %1.2f",result);
+}
+void Division(float a,float b){
+	float result;
+	result=a/b;
+	printf("\nEl resultado de su Division es: %1.2f",result);
 }
